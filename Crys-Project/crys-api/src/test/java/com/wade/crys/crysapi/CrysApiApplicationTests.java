b098aa6b28d261088e57597a6f18b503c836db99
Daniel Oana <daniel.oana@mambu.com>
2020-11-25 21:46:31 +0200

    Add project structure

---
+++ Crys-Project/crys-api/src/test/java/com/wade/crys/crysapi/CrysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wade.crys.crysapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrysApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
